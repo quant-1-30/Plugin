2c6ceed39403fafe9269d0b0e16591dd3c5ac182
hengxin.liu <hengxin.liu@inceptio.ai>
2025-09-23 15:40:48 +0800

    add logger in asset spider

--- spider/spider/crawlers/asset.py
+++ spider/spider/crawlers/asset.py
@@ -78,6 +78,7 @@ class Stock(BaseSpider):
     def preload(self, result): # used to filter
         # import pdb; pdb.set_trace()
         self.asset_first_trading = int(result[0][0])
+        self.logger.info(f"Preload Asset first_trading {result[0][0]}")
 
     # async def start(self):
     def start_requests(self):
